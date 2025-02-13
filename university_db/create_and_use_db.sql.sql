77e2c47ee77d741f9ccd23d6abaca647b049c7e7
BrendanPaabot <paabotbrendan_bsit@plmun.edu.ph>
2025-02-13 10:57:07 +0800

    commited su

--- university_db/create_and_use_db.sql.sql
+++ university_db/create_and_use_db.sql.sql
@@ -1,9 +1,9 @@
 USE UniversityDB;
 
 CREATE TABLE Students (
-   StudentID INT PRIMARY KEY AUTO_INCREMENT,
+   StudentID INT AUTO_INCREMENT,
    FirstName VARCHAR(50) NOT NULL,
    LastName VARCHAR(50) NOT NULL,
    EnrollmentDate DATE,
-   Email VARCHAR(50)
+   Email VARCHAR(100)
 );
